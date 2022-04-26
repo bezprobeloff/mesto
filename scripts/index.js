6c6d64d1f4915d4c8e7189691c76d477ac43d03c
Mikhail Belov <bezprobeloff@gmail.com>
2022-04-26 09:47:03 +0300

    refact: названия переменных и функций

--- scripts/index.js
+++ scripts/index.js
@@ -4,29 +4,28 @@ let profileJob = profileSection.querySelector('.profile__job');
 let profileEditButton = profileSection.querySelector('.profile__button-edit');
 
 let popupSection = document.querySelector('.popup');
-let closeButtonPopup = popupSection.querySelector('.popup__button-close');
+let buttonClosePopup = popupSection.querySelector('.popup__button-close');
 let formElement = popupSection.querySelector('.popup__form');
 let nameInput = formElement.querySelector("input[name='name']");
 let jobInput = formElement.querySelector("input[name='job']");
 
-function clickEditButtonHandler () {
+function togglePopup() {
   popupSection.classList.toggle('popup_opened');
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
 }
 
-function clickClosePopupButtonHandler () {
-  popupSection.classList.toggle('popup_opened');
+function clickEditButtonHandler () {
+  togglePopup();
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popupSection.classList.toggle('popup_opened');
+    togglePopup();
 }
 
 profileEditButton.addEventListener('click', clickEditButtonHandler);
-closeButtonPopup.addEventListener('click', clickClosePopupButtonHandler);
+buttonClosePopup.addEventListener('click', togglePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-
