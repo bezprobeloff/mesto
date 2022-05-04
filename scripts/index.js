c52cf29a2dbfeece70dfaf219044607bc7d5093f
Mikhail Belov <bezprobeloff@gmail.com>
2022-05-04 15:50:32 +0300

    feat: реализовано открытие попапа добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,51 @@
-let profileSection = document.querySelector('.profile');
-let profileName = profileSection.querySelector('.profile__name');
-let profileJob = profileSection.querySelector('.profile__job');
-let profileEditButton = profileSection.querySelector('.profile__button-edit');
+const profileSection = document.querySelector('.profile');
+const profileName = profileSection.querySelector('.profile__name');
+const profileJob = profileSection.querySelector('.profile__job');
+const profileEditButton = profileSection.querySelector('.profile__button-edit');
+const profileAddButton = profileSection.querySelector('.profile__button-add');
 
-let popupSection = document.querySelector('.popup');
-let buttonClosePopup = popupSection.querySelector('.popup__button-close');
-let formElement = popupSection.querySelector('.popup__form');
-let nameInput = formElement.querySelector(".popup__input_type_user-name");
-let jobInput = formElement.querySelector(".popup__input_type_user-job");
+// переменные попапа редактирования профиля
+const popupProfile = document.querySelector('.popup_type_edit-profile');
+const buttonClosePopupProfile = popupProfile.querySelector('.popup__button-close');
+const formProfile = popupProfile.querySelector('.popup__form');
+const nameInput = formProfile.querySelector(".popup__input_type_user-name");
+const jobInput = formProfile.querySelector(".popup__input_type_user-job");
 
-function togglePopup() {
-  popupSection.classList.toggle('popup_opened');
-}
+// переменные попапа добавления карточки
+const popupAddCard = document.querySelector('.popup_type_add-card');
+const buttonClosePopupAddCard = popupAddCard.querySelector('.popup__button-close');
+const formAddCard = popupAddCard.querySelector('.popup__form');
+const nameCardInput = formAddCard.querySelector(".popup__input_type_card-name");
+const linkCardInput = formAddCard.querySelector(".popup__input_type_card-link");
 
-function clickEditButtonHandler () {
-  togglePopup();
+const togglePopup = popup => popup.classList.toggle('popup_opened');
+
+const onButtonEdit = () => {
+  togglePopup(popupProfile);
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-function formSubmitHandler (evt) {
+const onButtonAdd = () => togglePopup(popupAddCard);
+
+const onButtonClosePopupProfile = () => togglePopup(popupProfile);
+
+const onButtonClosePopupAddCard = () => togglePopup(popupAddCard);
+
+const onFormSubmitProfile = evt => {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    togglePopup();
+    togglePopup(popupProfile);
 }
 
-profileEditButton.addEventListener('click', clickEditButtonHandler);
-buttonClosePopup.addEventListener('click', togglePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+// реакции на кнопки открытия попапов
+profileEditButton.addEventListener('click', onButtonEdit);
+profileAddButton.addEventListener('click', onButtonAdd)
+
+// реакции на кнопки закрытия попапов
+buttonClosePopupProfile.addEventListener('click', onButtonClosePopupProfile);
+buttonClosePopupAddCard.addEventListener('click', onButtonClosePopupAddCard);
+
+// отправка формы
+formProfile.addEventListener('submit', onFormSubmitProfile);
