02dec498be27389b45b87d99d748527715518bf1
Mikhail Belov <bezprobeloff@gmail.com>
2022-05-05 09:40:45 +0300

    feat: реализована логика лайка

--- scripts/index.js
+++ scripts/index.js
@@ -76,10 +76,16 @@ const onFormSubmitProfile = evt => {
     togglePopup(popupProfile);
 };
 
+const onButtonLike = (evt) => {
+  evt.target.classList.toggle('card__button-like_activated');
+};
+
 const renderCard = (card) => {
   const cardElement = templateCard.querySelector('.card').cloneNode(true);
   cardElement.querySelector('.card__image').src = card.link;
   cardElement.querySelector('.card__name').textContent = card.name;
+  cardElement.querySelector('.card__button-like')
+      .addEventListener('click', onButtonLike);
   cardsSection.prepend(cardElement);
 };
 
