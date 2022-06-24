b3ce8087500d5f1e8ae7e91dd0d7341d35acebde
Mikhail Belov <bezprobeloff@gmail.com>
2022-06-24 13:34:09 +0300

    feat: продолжение реструктуризации файлов

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import Card from './Card.js';
-import FormValidator from './FormValidator.js';
+import {initialCards} from '../utils/constants.js';
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
 
 const formSelectors = {
   inputSelector: '.popup__input',
