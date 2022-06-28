28c1e07d069fa426a78ba9e80b9d848d19f7fd81
Mikhail Belov <bezprobeloff@gmail.com>
2022-06-28 09:43:08 +0300

    refact: развая инициализация переменных класса

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,14 +3,14 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._image = this._popup.querySelector('.popup__view-image');
+    this._imageDesctiption = this._popup.querySelector('.popup__description');
   }
 
   open({ name, link}) {
-    const imageElement = this._popup.querySelector('.popup__view-image');
-    const imageDescription = this._popup.querySelector('.popup__description');
-    imageElement.src = link;
-    imageElement.alt = name;
-    imageDescription.textContent = name;
+    this._image.src = link;
+    this._image.alt = name;
+    this._imageDesctiption.textContent = name;
     super.open();
   }
 };
